8e367c040fe4f7f254248899b219f18195158589
Eduardodcsm <71696076+Eduardodcsm@users.noreply.github.com>
2024-01-24 20:25:36 +0000

    teste

--- pythonFolder/HelloWould.py
+++ pythonFolder/HelloWould.py
@@ -1,5 +1,5 @@
-#Example of comment
-print("Hello would")
-print("whats up")
-print('whats up')
+# Taking user input and storing it in the variable red_bucket
+red_bucket = input("What do you wanna put in the bucket? ")
 
+# Printing the content of red_bucket
+print(red_bucket)
