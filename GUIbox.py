fd99cfee1d75a03d29bd72ecd8acaa82f746700e
Eduardodcsm <71696076+Eduardodcsm@users.noreply.github.com>
2024-02-04 21:51:30 +0000

    nivi

--- GUIbox.py
+++ GUIbox.py
@@ -1,3 +1,4 @@
+#GUIbox 
 from tkinter import * 
 root = Tk() 
 
--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask, render_template, url_for, jsonify
-import subprocess
+# app.py
+
+from flask import Flask, render_template
+from subprocess import run
 
 app = Flask(__name__)
 
-@app.route('/run_python')
-def run_python():
-    try:
-        subprocess.run(['python', 'C:/Users/du57/Desktop/Eduardo/Portfolio/eduSite/app.py'])
-        return jsonify(success=True)
-    except Exception as e:
-        return jsonify(success=False, error=str(e))
+@app.route('/')
+def index():
+    return render_template('index.html')
 
+@app.route('/new_python_program')
+def new_python_program():
+    # Include the logic to execute your GUIbox.py here
+    # For simplicity, let's just return a message
+    return "Executing GUIbox.py"
+
+@app.route('/execute_gui_box')
+def execute_gui_box():
+    run(["python", "GUIbox.py"])
+    return "Executing GUIbox.py"
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
